b73f83e26d3e22f157b279b488c37fae6e167d23
abhiiii.iiii <abhijithviju2005@gmail.com>
2025-07-03 00:02:42 +0400

    pinata setup & generate hash

--- src/app/api/uploadToIPFS/route.ts
+++ src/app/api/uploadToIPFS/route.ts
@@ -1,37 +1,36 @@
-import {NextRequest , NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    try{
+    try {
         const body = await req.json();
 
-        const response  = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS",{
+        const response = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization : `Bearer ${process.env.PINIATA_JWT}`,
+                Authorization: `Bearer ${process.env.PINATA_JWT}`,
             },
-            body : JSON.stringify({
-                piniataMetadat : {
+            body: JSON.stringify({
+                pinataMetadata: {
                     name: body.BatchId || "Raw Material Data",
                 },
-                piniataContent : body,
+                pinataContent: body,
             }),
         });
 
-        const data =await response.json();
+        const data = await response.json();
 
-        if(!response.ok){
-            return NextResponse.json({success : false, error: data}, {status : 500});
+        if (!response.ok) {
+            return NextResponse.json({ success: false, error: data }, { status: 500 });
         }
 
         return NextResponse.json({
-            success : true,
-            cid : data.IpfsHash,
-            pinSize : data.Pinsize,
-            timestamp : data.timestamp,
-        }, {status : 200});
-    }
-    catch(err : any){
-        return NextResponse.json({success : false, error: err.message},{ status : 500});
+            success: true,
+            cid: data.IpfsHash,
+            pinSize: data.PinSize,
+            timestamp: data.Timestamp,
+        }, { status: 200 });
+    } catch (err: any) {
+        return NextResponse.json({ success: false, error: err.message }, { status: 500 });
     }
 }
