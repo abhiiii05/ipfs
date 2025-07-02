f58ecb38436200e94404777870a3d7752f17da5e
abhiiii.iiii <abhijithviju2005@gmail.com>
2025-07-02 13:16:05 +0400

    shad cn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center ">
-      <h1 className="text-4xl font-bold text-white">
-        Hello Tailwind!
-      </h1>
+    <div>
     </div>
   );
 }
