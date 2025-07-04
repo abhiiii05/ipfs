7422bdf33a16f360054e31d0a23adb11da3d8b92
abhiiii.iiii <abhijithviju2005@gmail.com>
2025-07-05 00:14:22 +0400

    deploy -1

---
+++ blockchain/test/PharmaData.ts
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { PharmaceuticalData } from "../typechain-types";
+
+describe("PharmaceuticalData", function () {
+    let pharmaData: PharmaceuticalData;
+    let owner: any;
+    let addr1: any;
+    let addr2: any;
+
+    beforeEach(async function () {
+        // Get signers
+        [owner, addr1, addr2] = await ethers.getSigners();
+
+        // Deploy contract
+        const PharmaceuticalData = await ethers.getContractFactory("PharmaceuticalData");
+        pharmaData = await PharmaceuticalData.deploy();
+        await pharmaData.waitForDeployment();
+    });
+})
